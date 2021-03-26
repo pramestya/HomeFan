c4cdf8b2e1eebc938d02b595be4850bc1f760eaf
pramestya <pramestya.yohana.tif`9@polban.ac.id>
2021-03-26 21:18:02 +0700

    First commit

---
+++ app/src/main/java/com/example/homefan/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.homefan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
+import android.os.Bundle;
+import android.view.animation.LinearInterpolator;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.Switch;
+import android.widget.ToggleButton;
+
+import static android.graphics.Color.*;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ToggleButton toggleButton = (ToggleButton) findViewById(R.id.toggleButton);
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        ObjectAnimator rotateAnimator;
+        Switch switchLights = (Switch) findViewById(R.id.switchLights);
+        SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar);
+        final int SPEED[] = {0, 5000, 3000, 1000};
+        GradientDrawable gd = new GradientDrawable();
+
+        gd.setShape(GradientDrawable.RECTANGLE);
+        gd.setGradientType(GradientDrawable.RADIAL_GRADIENT);
+        gd.setGradientRadius(330);
+
+        switchLights.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    gd.setColors(new int[]{ Color.YELLOW , Color.TRANSPARENT });
+                    imageView.setBackground(gd);
+                } else {
+                    imageView.setBackgroundColor(Color.TRANSPARENT);
+                }
+            }
+        });
+
+        rotateAnimator=ObjectAnimator.ofFloat(imageView, "rotation", 0,360);
+        rotateAnimator.setDuration(SPEED[0]);
+        rotateAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        rotateAnimator.setInterpolator(new LinearInterpolator());
+
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    rotateAnimator.start();
+                } else {
+                    rotateAnimator.end();
+                }
+            }
+        });
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if(progress == 0){
+                    rotateAnimator.setDuration(SPEED[0]);
+                }else if(progress == 1){
+                    rotateAnimator.setDuration(SPEED[1]);
+                }else if(progress == 2){
+                    rotateAnimator.setDuration(SPEED[2]);
+                }else{
+                    rotateAnimator.setDuration(SPEED[3]);
+                }
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+
+}
